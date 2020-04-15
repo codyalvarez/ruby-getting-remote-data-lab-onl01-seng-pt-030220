23587b819fdb6771e2ef7ad32aeae0292dd85022
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-15 14:26:28 +0000

    Automatically backed up by Learn

--- spec/get_requester_spec.rb
+++ spec/get_requester_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'net/http'
+
 
 URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
 JSON_STRING = "[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]"
